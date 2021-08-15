52e166b44e42170dd9ece59211b0135b2bf19748
Oyinbo David Bayode <dev.davexoyinbo@gmail.com>
2021-08-15 13:41:06 +0100

    Property deleted observer

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Property extends Model
 {
@@ -12,10 +13,31 @@ class Property extends Model
 
     protected $guarded = [];
 
+
+
     protected $casts = [
         'gallery' => 'array',
         'other_features' => 'array'
     ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::deleted(function ($property) {
+            $previousGallery = $property->gallery;
+            //delete previous files in gallery
+            if ($previousGallery && count($previousGallery) > 0) {
+                foreach ($previousGallery as $previousUrl) {
+                    $deletePath = preg_replace("/^\/storage/", "", $previousUrl);
+                    Storage::disk("public")->delete($deletePath);
+                }
+            }
+        });
+    }
 
     public function user()
     {
